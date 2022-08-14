56668d9553d5cb773142f3798bbc0bcc08daab86
Szilard Zrupko <szilard@Szilards-MacBook-Air.local>
2022-08-14 14:26:43 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0cbd5fb1-3636-449b-a3e1-5a115eb9ba5a';
+export const MODAL_CLOSE_SEC = 2.5;
